bd798e8d66bf069830eae22bac8ac96ccd28387d
Gaston <Gastonwilliamsonn@gmail.com>
2022-03-26 19:44:19 -0500

    index.html

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let count = 0;
 let saveEl = document.getElementById("save-el");
 let countEL = document.getElementById("count-el");
+
+let myAge = 32;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
